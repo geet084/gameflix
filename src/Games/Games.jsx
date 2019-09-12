0720d6dedb4f55f59792e12f5dcc7a108cfcb53f
Travis Gee <geet084@gmail.com>
2019-09-12 10:12:39 -0600

    Updates Games to use api data

--- src/Games/Games.jsx
+++ src/Games/Games.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import Game from '../Game/Game';
-const gamesList = new Array(25).fill('game');
-//depends on how many boxes you have on screen 7 for full screen
-const empty = new Array(7).fill('');
 
-const games = gamesList.map((game, i) => {
-  return <Game key={i} game={game + ' ' + i} />
-})
-const blankGames = empty.map(i => <Game key={i} />)
+export const Games = ({ games }) => {
+  const gameList = games.map(game => <Game key={game.id} {...game} />)
+  const blankGames = new Array(2).fill('').map((none, i) => <Game key={i} />)
 
-export const Games = () => {
   return (
     <article>
-      {games}
+      {games[0] && gameList}
       {blankGames}
     </article>
   )
