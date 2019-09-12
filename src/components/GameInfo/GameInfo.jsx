d8ab70f5d4a798199daa1041ed0814420c9673ef
Travis Gee <geet084@gmail.com>
2019-09-12 14:27:58 -0600

    Adds initial game info data and styling

--- src/components/GameInfo/GameInfo.jsx
+++ src/components/GameInfo/GameInfo.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 export const GameInfo = (props) => {
+  const { toggle, name, images, min_players, max_players, min_playtime, max_playtime, min_age, description } = props;
 
   return (
-    <section onClick={() => props.toggle(props.name)}>
-      <h1>
-        {props.name}
-      </h1>
+    <section className="info">
+      <span className="back" onClick={() => toggle(name)}>X</span>
+      <h2 className="title">{name}</h2>
+      <img src={images.small} alt={name}/>
+      <p>Players: {min_players} - {max_players}</p>
+      <p>Playtime: {min_playtime} - {max_playtime} minutes</p>
+      <p>Minimum age: {min_age}</p>
+      <p>{description}</p>
     </section>
   )
 }
