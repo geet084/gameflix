57f0ab07219415a075dd10729e0035f7a7e5aa5c
Travis Gee <geet084@gmail.com>
2019-09-12 10:10:08 -0600

    Adds initial api fetch call with hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Games from '../Games/Games';
 import Header from '../Header/Header';
 
-function App() {
+const App = () => {
+  const [hasError, setErrors] = useState('')
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    const url = process.env.REACT_APP_INITIAL_FETCH_URL;
+
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw Error(response.statusText);
+        const result = await response.json();
+        setGames(result.games);
+      } catch ({ message }) {
+        setErrors(`Oops! It looks like there was an issue: ${message}`);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <main className="App">
+      {hasError !== "" && <h3 className="err-msg">{hasError}</h3>}
       <Header />
-      <Games />
+      <Games games={games} />
     </main>
   );
 }
