3dc71816581775b193024c7430a3e87e736e4fa2
Travis Gee <geet084@gmail.com>
2019-09-13 17:54:54 -0600

    Adds a method for query param assembly

--- src/App/App.js
+++ src/App/App.js
@@ -23,15 +23,33 @@ const App = () => {
   const fetchData = async () => {
     const url = process.env.REACT_APP_SEARCH_URL;
     const client = process.env.REACT_APP_CLIENT;
+    let params = getParams();
 
     try {
-      const response = await fetch(url + client);
+      const response = await fetch(url + params + client);
       if (!response.ok) throw Error(response.statusText);
       const result = await response.json();
       setGames(result.games);
     } catch ({ message }) {
       setErrors(`Oops! It looks like there was an issue: ${message}`);
     }
+  }
+
+  const getParams = () => {
+    const { name, min_players, max_players, max_playtime } = searchTerms;
+    let params = ''
+
+    if (name !== '') params += `name=${name}&`;
+
+    if (min_players > 0) params += `gt_min_players=${min_players - 1}&`;
+
+    if (max_players === 6) params += `gt_max_players=${max_players}&`;
+    else if (max_players > 0 && max_players < 6) params += `max_players=${max_players}&`;
+
+    if (max_playtime > 120) params += `min_playtime=120&`;
+    else if (max_playtime > 0 && max_playtime <= 120) params += `max_playtime=${max_playtime}&`
+
+    return params;
   }
 
   const handleSearch = ({ target }) => {
