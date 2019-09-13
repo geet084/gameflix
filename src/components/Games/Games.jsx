b46858dbd372a77d65bc9322f97e6fbda0816363
Travis Gee <geet084@gmail.com>
2019-09-13 17:37:21 -0600

    Refactors games to use class name for styling

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -16,7 +16,7 @@ export const Games = ({ games }) => {
 
 
   return (
-    <article>
+    <article className="games">
       {selectedGame === '' ? gameTiles : <GameInfo toggle={toggle} {...game} />}
       {blankTiles}
     </article>
