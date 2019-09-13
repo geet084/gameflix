192c2b6b5f9e231e52b37671ecea04e79ce75494
Travis Gee <geet084@gmail.com>
2019-09-13 17:35:57 -0600

    Adds search options to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 
-export const Header = () => {
+export const Header = ({ handleSearch, handleReset, name, num, min_players, max_players, max_playtime, min_rating }) => {
+  const players = ['0', '1', '2', '3', '4', '5', '6+']
+  const playTime = ['0', '15 max', '30 max', '45 max', '60 max', '90 max', '120 max', 'Over 120']
+  const stars = ['0 stars & up', '1 star & up', '2 stars & up', '3 stars & up', '4 stars & up']
+
   return (
     <header>
       <h1 className="title">GAMEFLIX</h1>
+      <p> results:  {num}</p>
+      <div className="options">
+        <button onClick={handleReset}>Reset</button>
+        <span>
+          <input id="name" type="text" value={name} onChange={handleSearch} placeholder="Search" />
+        </span>
+        <Dropdown id="min_players" name="Min Players" value={min_players} range={players} handleSearch={handleSearch} />
+        <Dropdown id="max_players" name="Max Players" value={max_players} range={players} handleSearch={handleSearch} />
+        <Dropdown id="max_playtime" name="Playtime (mins)" value={max_playtime} range={playTime} handleSearch={handleSearch} />
+        <Dropdown id="min_rating" name="Avg. User Rating" value={min_rating} range={stars} handleSearch={handleSearch} />
+      </div>
     </header>
   )
 }
