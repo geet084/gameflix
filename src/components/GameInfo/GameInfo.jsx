2dde49bd2cdfc8905d8e0012fa574b7c2468090e
Travis Gee <geet084@gmail.com>
2019-09-14 12:58:25 -0600

    Adds more data to game info page

--- src/components/GameInfo/GameInfo.jsx
+++ src/components/GameInfo/GameInfo.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export const GameInfo = (props) => {
-  const { toggle, name, images, min_players, max_players, min_playtime, max_playtime, min_age, description } = props;
+  const { toggle, name, images, min_players, max_players, min_playtime, max_playtime, min_age, description, price, msrp, discount, primary_publisher, average_user_rating, historical_low_price } = props;
+
+  const maxDescLength = 250;
+  let shortDesc = '';
+  description.split(' ').forEach(word => {
+    if (`${shortDesc} ${word}`.length <= maxDescLength) shortDesc += ` ${word}`;
+  })
 
   return (
     <section className="info">
@@ -10,8 +16,19 @@ export const GameInfo = (props) => {
       <img src={images.small} alt={name}/>
       <p>Players: {min_players} - {max_players}</p>
       <p>Playtime: {min_playtime} - {max_playtime} minutes</p>
+      <p>Average User Rating: {average_user_rating.toFixed(2)}</p>
       <p>Minimum age: {min_age}</p>
-      <p>{description}</p>
+      <p>Price: ${price} - {(discount*100).toFixed(0)}% off</p>
+      <p>MSRP: ${msrp}</p>
+      <p>Historical Low Price: ${historical_low_price}</p>
+      <p>Publisher: {primary_publisher}</p>
+      <div>
+        <p>Mechanics: </p>
+      </div>
+      <div>
+        <p>Categories: </p>
+      </div>
+      <p className="desc ellipsis">{shortDesc} ...</p>
     </section>
   )
 }
