44c2fb5c0ad8c69067355c1dcc1536022afdbd2d
Travis Gee <geet084@gmail.com>
2019-09-18 13:27:10 -0600

    Adds checks for ensuring props are valid for rendering

--- src/components/GameInfo/GameInfo.jsx
+++ src/components/GameInfo/GameInfo.jsx
@@ -5,14 +5,14 @@ export const GameInfo = (props) => {
 
   const maxDescLength = 250;
   let shortDesc = '';
-  description.split(' ').forEach(word => {
+  description && description.split(' ').forEach(word => {
     if (`${shortDesc} ${word}`.length <= maxDescLength) shortDesc += ` ${word}`;
   })
 
   const disc = discount && (discount * 100).toFixed(0) > 0
     ? <span className="less">{Math.abs((discount * 100).toFixed(0))}% off</span>
     : <span className="more">{Math.abs((discount * 100).toFixed(0))}% more</span>
-  
+
   const lowComp = (((price / historical_low_price) - 1) * 100).toFixed(0) < 0.01
     ? <span className="less">{Math.abs((((price / historical_low_price) - 1) * 100).toFixed(0))}% off</span>
     : <span className="more">{Math.abs((((price / historical_low_price) - 1) * 100).toFixed(0))}% more</span>
@@ -21,22 +21,22 @@ export const GameInfo = (props) => {
     <section className="info">
       <span className="back" onClick={() => toggle(name)}>X</span>
       <h2 className="title">{name}</h2>
-      <img src={images.small} alt={name}/>
-      <p>Players: {min_players} - {max_players}</p>
-      <p>Playtime: {min_playtime} - {max_playtime} minutes</p>
-      <p>Average User Rating: {average_user_rating.toFixed(2)}</p>
-      <p>Minimum age: {min_age}</p>
-      <p>Price: ${price}</p>
-      <p>MSRP: ${msrp} &nbsp;&nbsp;&nbsp; {disc}</p>
-      <p>Historical Low Price: ${historical_low_price} &nbsp;&nbsp;&nbsp; {lowComp}</p>
-      <p>Publisher: {primary_publisher}</p>
+      <img src={images.small} alt={name} />
+      {min_players && max_players && <p>Players: {min_players} - {max_players}</p>}
+      {min_playtime && max_playtime && <p>Playtime: {min_playtime} - {max_playtime} minutes</p>}
+      {average_user_rating && <p>Average User Rating: {average_user_rating.toFixed(2)}</p>}
+      {min_age && <p>Minimum age: {min_age}</p>}
+      {price && <p>Price: ${price}</p>}
+      {msrp && <p>MSRP: ${msrp} &nbsp;&nbsp;&nbsp; {disc}</p>}
+      {historical_low_price && <p>Historical Low Price: ${historical_low_price} &nbsp;&nbsp;&nbsp; {lowComp}</p>}
+      {primary_publisher && <p>Publisher: {primary_publisher}</p>}
       <div>
         <p>Mechanics: </p>
-      </div>
+        </div>
       <div>
-        <p>Categories: </p>
-      </div>
-      <p className="desc ellipsis">{shortDesc} ...</p>
+      <p>Categories: </p>
+    </div>
+    <p className="desc ellipsis">{shortDesc} ...</p>
     </section>
   )
 }
