b8e836c1821ddeea67dff284130e827693c3b72b
Travis Gee <geet084@gmail.com>
2019-09-18 16:28:20 -0600

    Updates selected game with readable mechanics and categories

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import GameTile from '../GameTile/GameTile';
 import GameInfo from '../GameInfo/GameInfo';
 
-export const Games = ({ games }) => {
+export const Games = ({ games, mechanics, categories }) => {
   const [selectedGame, setselectedGame] = useState('')
 
   const toggle = (gameName) => {
@@ -14,6 +14,10 @@ export const Games = ({ games }) => {
   const blankTiles = new Array(4).fill('').map((none, i) => <span key={i} className="empty"></span>)
   const game = games.find(game => game.name === selectedGame)
 
+  if (game) {
+    game.mechanics = game.mechanics.map(mech => mechanics.find(data => data.id === mech.id).name)
+    game.categories = game.categories.map(cat => categories.find(data => data.id === cat.id).name)
+  }
 
   return (
     <article className="games">
