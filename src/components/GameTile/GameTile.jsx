eaad1bafe39911f6e51fbab9d7f9e9c796b18991
Travis Gee <geet084@gmail.com>
2019-09-19 16:15:42 -0600

    Adds fave icon and toggle to game tile

--- src/components/GameTile/GameTile.jsx
+++ src/components/GameTile/GameTile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function validateHasValue(props) {
   let updatedProps = { ...props }
-  const values = [ 'min_players', 'max_players', 'max_playtime', 'average_user_rating'];
+  const values = ['min_players', 'max_players', 'max_playtime', 'average_user_rating'];
 
   values.forEach(key => {
     if (!updatedProps[key]) updatedProps[key] = 0;
@@ -10,11 +10,17 @@ function validateHasValue(props) {
   return updatedProps;
 }
 
+
 export const GameTile = (props) => {
-  const { images, name, min_age, min_playtime, toggle } = props;
+  const { images, name, min_playtime, toggle, handleFaves, id, fave } = props;
   const { min_players, max_players, max_playtime, average_user_rating } = validateHasValue(props);
   const maxNameLength = 27
   const style = { background: `url(${images.small}) no-repeat center` };
+
+  const toggleFaves = (e) => {
+    e.stopPropagation();
+    handleFaves(id);
+  }
 
   let shortName = '';
   name.split(' ').forEach(word => {
@@ -23,11 +29,11 @@ export const GameTile = (props) => {
 
   return (
     <section className="game" style={style} onClick={() => toggle(name)}>
-      {<h4 className="title">{name.length > maxNameLength ? `${shortName} ...` : name}</h4>}
+      <h4 className="title">{name.length > maxNameLength ? `${shortName} ...` : name}</h4>
       <div className="btm-text">
         <p>
           <span> {min_players} - {max_players} <i className="fas fa-users"></i> </span>
-          <span> {min_age}+&nbsp; </span>
+          <i className={`far fa-heart${fave ? ' fave' : ''}`} onClick={toggleFaves}></i>
         </p>
         <p>
           <span> <i className="far fa-clock"></i> {min_playtime} - {max_playtime} </span>
